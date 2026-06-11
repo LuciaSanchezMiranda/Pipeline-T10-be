4e4defc3a97915a6936e7f44c011d305673ba01c
LuciaSanchez <lucia.sanchez@vallegrande.edu.pe>
2026-06-11 03:24:54 -0500

    docs(sale-detail): add comments and JavaDocs

--- src/main/java/vallegrande/luSanchezMiranda/repository/SaleDetailRepository.java
+++ src/main/java/vallegrande/luSanchezMiranda/repository/SaleDetailRepository.java
@@ -3,6 +3,15 @@ package vallegrande.luSanchezMiranda.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import vallegrande.luSanchezMiranda.model.SaleDetail;
 
+/**
+ * Repositorio JPA para acceder a los datos de la tabla SALE_DETAILS.
+ */
 public interface SaleDetailRepository extends JpaRepository<SaleDetail, Integer> {
+    
+    /**
+     * Elimina todos los detalles de venta asociados a un ID de venta específico.
+     * Utilizado para limpiezas en cascada manuales si son necesarias.
+     * @param saleId Identificador de la venta padre.
+     */
     void deleteBySale_SaleId(Integer saleId);
 }
